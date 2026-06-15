ab37c5383422abf24f78555e8aa15a64db069112
johnfrancis111 <nnopuechichijioke@gmail.com>
2026-06-15 23:35:27 +0100

    fixed assignments

---
+++ assignment 1/script.js
@@ -0,0 +1,15 @@
+// Create variables
+let a = 10;
+let b = 5;
+
+// Add numbers
+let sum = a + b;
+console.log("Sum:", sum);
+
+// Compare values
+console.log("a > b:", a > b);
+console.log("a == b:", a == b);
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 4));
